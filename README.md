f42fc4960d3e94f718627a2947898938c9350f8e
Mohamed Labib <m.ali.labib@gmail.com>
2016-04-11 02:20:22 +0200

    Update project deploy script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # basic-blog
 A basic blog using symfony2 framework
+
+* blog CRUD is accessible via `/blog`
+
+#### Run script to build project dependencies
+run `./deploy.sh`
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+# update vendor libraries
+composer update -n
+# update DB schema
 php bin/console doctrine:schema:update --force
+# prepare public assets
 npm install -g bower
 php bin/console assets:install --symlink
